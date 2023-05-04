5cd277591cb14b32a129200644164afe106e82e4
kenghoufong1 <kenghoufong1@gmail.com>
2023-05-03 20:10:23 -0700

    fixed bugs for deploying pages

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ import Skill from './components/Skill';
 import Contact from './components/Contact';
 import MyNavbar from './components/Navbar';
 import Github from './components/github';
+import employee from "./assets/employee.png"
+import freshg from "./assets/freshg.webp"
+import notetaker from "./assets/note taker.jpg"
+import stone from "./assets/stone background.jpg"
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -17,25 +21,25 @@ gsap.registerPlugin(ScrollTrigger);
 function App() {
   const projects = [
     {
-      imageSrc: "./freshg.webp",
+      imageSrc: freshg,
       title: "Savr App",
       text: "An app that let's you find all local grocery deals available",
       link:"https://savr-app.herokuapp.com/"
     },
     {
-      imageSrc: "./stone background.jpg",
+      imageSrc: stone,
       title: "Wellness Tracker",
       text: "Keep track of all your daily exercise in just the click of a few button",
       link:"https://wellness-tracker206.herokuapp.com/"
     },
     {
-      imageSrc: "./employee.png",
+      imageSrc: employee,
       title: "The Tech Blog",
       text: "A place where all techer can share their thoughts!",
       link:"https://the-tech-blog206.herokuapp.com/"
     },
     {
-      imageSrc: "./note taker.jpg",
+      imageSrc: notetaker,
       title: "Note Taker",
       text: "A place where you can keep track of all your daily rountines",
       link:"https://note-taker-205.herokuapp.com/"
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import '../header.css';
 import React from 'react';
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useRef,useEffect } from "react";
 
 function Header() {
@@ -20,7 +19,6 @@ function Header() {
             <br></br>
             <h1>About Me</h1>
             <p>Keng is a full Stack web developer . Earned a certificate in Full Stack Web Development from the University Of Washington Coding Boot Camp. Innovative problem-solver who is passionate about developing apps . Strengths in creativity, teamwork, and building projects from ideation to execution. </p>
-            {/* <button className="cta">Contact Me</button> */}
         </div>
     );
 
